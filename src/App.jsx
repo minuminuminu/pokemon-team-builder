94fa65935137b6079ac9b342535366a5311d48e0
Minu <minu.kim2003@gmail.com>
2021-11-03 14:56:47 +0100

    add simple navbar with logo and h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 /* LINE BREAK FOR COMPONENTS/PAGES AND LIBRARIES */
 import { NavBar } from "./components/NavBar";
 import { ErrorPage } from "./pages/ErrorPage";
 import { FaqPage } from "./pages/FaqPage";
 import { HomePage } from "./pages/HomePage";
 
-const FullPage = styled.div`
-  background-color: #f5f5f5;
-  width: 100vw;
-  height: 100vh;
+const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin:0;
+    box-sizing: border-box;
+  }
+  body{
+    background-color:#f0f0f0;
+    font-family: 'Open Sans', sans-serif;
+  }
 `;
 
 function App() {
   return (
-    <FullPage>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="faq">
-            <FaqPage />
-          </Route>
-        </Switch>
-        <Route path="*">
-          <ErrorPage />
+    <BrowserRouter>
+      <GlobalStyle />
+      <NavBar />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
         </Route>
-      </BrowserRouter>
-    </FullPage>
+        <Route path="faq">
+          <FaqPage />
+        </Route>
+      </Switch>
+      <Route path="*">
+        <ErrorPage />
+      </Route>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
+const NavBarContainer = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavBarText = styled.h1`
+  font-size: x-large;
+  margin-left: 1vw;
+`;
+
 export const NavBar = () => {
-  return <div></div>;
+  return (
+    <NavBarContainer>
+      <div>
+        <img
+          src="../pokemon-logo.png"
+          alt="Pokemon Logo"
+          className="nav-image"
+        />
+      </div>
+      <NavBarText>Team Builder</NavBarText>
+    </NavBarContainer>
+  );
 };
