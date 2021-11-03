a7ecf1cc15f5dc1988bfd59c9d833e061d753157
Minu Kim <minu.kim2003@gmail.com>
2021-11-03 23:21:53 +0100

    add fetch pokemon

--- src/components/PokemonContainer.jsx
+++ src/components/PokemonContainer.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 const Container = styled.div`
   position: absolute;
@@ -9,5 +10,42 @@ const Container = styled.div`
 `;
 
 export const PokemonContainer = () => {
-  return <Container></Container>;
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const rawData = await fetch("https://pokeapi.co/api/v2/pokemon?limit=15");
+      const jsonData = await rawData.json();
+
+      const jsonDataResults = jsonData.results;
+      const results = [];
+
+      for (let i = 0; i < jsonDataResults.length; i++) {
+        results.push({
+          name: jsonDataResults[i].name,
+          url: jsonDataResults[i].url,
+          id: i + 1,
+        });
+      }
+
+      setPokemons(results);
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <Container>
+      <div>
+        {pokemons.map((pokemon) => {
+          return (
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              key={pokemon.id}
+            />
+          );
+        })}
+      </div>
+    </Container>
+  );
 };
