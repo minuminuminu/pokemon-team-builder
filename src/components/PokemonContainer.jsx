bf53a0cb05f6d2ee9c3b874aa3018eda7e355c6c
Minu <minu.kim2003@gmail.com>
2021-11-03 18:01:21 +0100

    color change

--- src/components/PokemonContainer.jsx
+++ src/components/PokemonContainer.jsx
@@ -5,7 +5,7 @@ const Container = styled.div`
   bottom: 5%;
   width: 70vw;
   height: 80vh; //maybe auto for expanding per pokemon row??? idk
-  border: 1px solid #818181;
+  border: 1px solid #c7c7c7;
 `;
 
 export const PokemonContainer = () => {
